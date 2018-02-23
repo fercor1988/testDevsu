f4e303e10d548d3dd3c0b4f1e004acc83bda5a9b
ba0100061e <ba0100061e@ba0100061e.austro.grpfin>
2018-02-23 15:36:11 -0500

    Add Json data in response

---
+++ src/main/java/com/baustro/testdevsu/bean/ObjMessage.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.baustro.testdevsu.bean;
+
+/**
+ *
+ * @author ba0100061e
+ */
+public class ObjMessage {
+
+    String message;
+
+    public ObjMessage(String india) {
+
+        this.message = india;
+    }
+
+    public String getmessage() {
+        return message;
+    }
+
+    public void setmessage(String message) {
+        this.message = message;
+    }
+}
--- src/main/java/com/baustro/testdevsu/controller/Rest_Controller.java
+++ src/main/java/com/baustro/testdevsu/controller/Rest_Controller.java
@@ -4,19 +4,32 @@
  * and open the template in the editor.
  */
 package com.baustro.testdevsu.controller;
+
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
+import com.baustro.testdevsu.bean.ObjMessage;
+import java.util.ArrayList;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequestMapping("/hello")
 public class Rest_Controller {
- @RequestMapping(value = "/{name}", method = RequestMethod.GET)
- public String hello(HttpServletRequest request,@PathVariable String name) {
-  String result="Hello "+name+ " from "+ request.getRemoteHost();
-  return result;
- }
+
+    @RequestMapping(value = "/{name}", method = RequestMethod.GET, headers = "Accept=application/json")
+    public List<ObjMessage> hello(HttpServletRequest request, @PathVariable String name) {
+        String host = request.getRemoteHost();
+        List<ObjMessage> listOfMessaje = new ArrayList<ObjMessage>();
+        listOfMessaje = createObjMessageList(name, host);
+        return listOfMessaje;
+    }
+
+    public List<ObjMessage> createObjMessageList(String name, String host) {
+        ObjMessage datatest = new ObjMessage("Hello " + name + " from " + host);
+        List<ObjMessage> listOfCountries = new ArrayList<ObjMessage>();
+        listOfCountries.add(datatest);
+        return listOfCountries;
+    }
 }
